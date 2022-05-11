69992f71ba9945053e9da0a0034dabcf61b4cb75
Emre ŞURK <emre.surk@yapikredi.com.tr>
2022-05-11 16:37:19 +0300

    creating modals.

---
+++ ResumeApp/Modals/EducationDetailsModal.swift
@@ -0,0 +1,15 @@
+//
+//  EducationDetailsModal.swift
+//  ResumeApp
+//
+//  Created by Emre on 11.05.2022.
+//
+
+import Foundation
+
+
+class EducationDetailsModal: Codable {
+    var className : String?
+    var passingYear : Int?
+    var percentageCGPA : Float?
+}
--- ResumeApp/Modals/ResumeModal.swift
+++ ResumeApp/Modals/ResumeModal.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 class ResumeModal : Codable {
-    
+    var phoneNumber : String?
+    var emailAdress : String?
+    var residenceAddress : String?
+    var careerObjective : String?
+    var totalYearsOfExperience : Int?
+    var workSummary : String?
+    var skills : [String] = []
+    var educationDetails : [EducationDetailsModal] = []
+    var projectDetails : [ProjectDetailsModal] = []
 }
